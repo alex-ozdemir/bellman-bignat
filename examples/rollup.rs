4a9b85eec1ebf28d73fbc56117edbe063e198885
Alex Ozdemir <aozdemir@hmc.edu>
2019-09-04 11:42:42 -0700

    Add initial/final state to rollup examples

--- examples/rollup.rs
+++ examples/rollup.rs
@@ -4,9 +4,11 @@ extern crate rand;
 extern crate sapling_crypto;
 
 use bellman_bignat::bignat::nat_to_limbs;
-use bellman_bignat::set::{Set, SetParams, SetInputs};
 use bellman_bignat::group::RsaGroup;
-use bellman_bignat::rsa_set::NaiveExpSet;
+use bellman_bignat::hash::helper::hash_to_prime;
+use bellman_bignat::hash::HashDomain;
+use bellman_bignat::rsa_set::{ExpSet, NaiveExpSet};
+use bellman_bignat::set::{Set, SetInputs, SetParams};
 use num_bigint::BigUint;
 use sapling_crypto::poseidon::bn256::Bn256PoseidonParams;
 
@@ -15,13 +17,16 @@ use std::time::Instant;
 
 // From https://en.wikipedia.org/wiki/RSA_numbers#RSA-2048
 const RSA_2048: &str = "25195908475657893494027183240048398571429282126204032027777137836043662020707595556264018525880784406918290641249515082189298559149176184502808489120072844992687392807287776735971418347270261896375014971824691165077613379859095700097330459748808428401797429100642458691817195118746121515172654632282216869987549182422433637259085141865462043576798423387184774447920739934236584823824281198163815010674810451660377306056201619676256133844143603833904414952634432190114657544454178424020924616515723350778707749817125772467962926386356373289912154831438167899885040445364023527381951378636564391212010397122822120720357";
-const RSA_SIZE: usize= 2048;
+const RSA_SIZE: usize = 2048;
 const ELEMENT_SIZE: usize = 5;
 
 fn main() {
     color_backtrace::install();
 
-    let n_swaps = std::env::args().nth(1).and_then(|a| usize::from_str(&a).ok()).expect("Provide the number of transactions as the first argument");
+    let n_swaps = std::env::args()
+        .nth(1)
+        .and_then(|a| usize::from_str(&a).ok())
+        .expect("Provide the number of transactions as the first argument");
 
     let hash = Bn256PoseidonParams::new::<sapling_crypto::group_hash::Keccak256Hasher>();
     use rand::thread_rng;
@@ -61,10 +66,14 @@ fn main() {
 
     let generate_params_end = Instant::now();
 
-    println!("Done with parameters, duration {:?}", generate_params_end - generate_params_start);
+    println!(
+        "Done with parameters, duration {:?}",
+        generate_params_end - generate_params_start
+    );
 
     let pvk = prepare_verifying_key(&params.vk);
     println!("Done with key");
+    let hash_params = Bn256PoseidonParams::new::<sapling_crypto::group_hash::Keccak256Hasher>();
 
     // Create a groth16 proof with our parameters.
     let circuit = Set {
@@ -73,7 +82,7 @@ fn main() {
             n_swaps,
             n_swaps,
             ELEMENT_SIZE,
-            &Bn256PoseidonParams::new::<sapling_crypto::group_hash::Keccak256Hasher>(),
+            &hash_params,
             RSA_SIZE,
             RsaGroup {
                 g: BigUint::from(2usize),
@@ -93,6 +102,47 @@ fn main() {
         },
     };
 
+    let ins = circuit.inputs.as_ref().unwrap();
+    let initial_set = ins.initial_state.clone();
+    let final_set = {
+        let mut t = initial_set.clone();
+        let to_remove = ins
+            .to_remove
+            .iter()
+            .map(|data| -> BigUint {
+                hash_to_prime::<Bn256>(
+                    data.as_slice(),
+                    &HashDomain {
+                        n_bits: circuit.params.n_bits_elem,
+                        n_trailing_ones: 1,
+                    },
+                    &hash_params,
+                )
+                .unwrap()
+                .0
+            })
+            .collect::<Vec<_>>();
+        let to_insert = ins
+            .to_insert
+            .iter()
+            .map(|data| -> BigUint {
+                hash_to_prime::<Bn256>(
+                    data.as_slice(),
+                    &HashDomain {
+                        n_bits: circuit.params.n_bits_elem,
+                        n_trailing_ones: 1,
+                    },
+                    &hash_params,
+                )
+                .unwrap()
+                .0
+            })
+            .collect::<Vec<_>>();
+        t.remove_all(&to_remove);
+        t.insert_all(to_insert);
+        t
+    };
+
     let prover_start = Instant::now();
 
     let proof = create_random_proof(circuit, &params, rng).unwrap();
@@ -102,7 +152,15 @@ fn main() {
     use sapling_crypto::bellman::pairing::bn256::Bn256;
     use sapling_crypto::bellman::pairing::ff::ScalarEngine;
     let mut inputs: Vec<<Bn256 as ScalarEngine>::Fr> = nat_to_limbs(&group.g, 32, 64);
-    inputs.extend(nat_to_limbs::<<Bn256 as ScalarEngine>::Fr>(&group.m, 32, 64));
+    inputs.extend(nat_to_limbs::<<Bn256 as ScalarEngine>::Fr>(
+        &group.m, 32, 64,
+    ));
+    inputs.extend(nat_to_limbs::<<Bn256 as ScalarEngine>::Fr>(
+        &initial_set.digest(), 32, 64,
+    ));
+    inputs.extend(nat_to_limbs::<<Bn256 as ScalarEngine>::Fr>(
+        &final_set.digest(), 32, 64,
+    ));
 
     println!("verified {:?}", verify_proof(&pvk, &proof, &inputs));
 }
