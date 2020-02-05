40d79775dd2205d84ceddb06c80c9ae5e6e86dae
Alex Ozdemir <aozdemir@hmc.edu>
2020-02-05 11:35:32 -0800

    Some timings data. Not complete.
    In particular, missing most RSA data, and not including accumulator
    initialization times for either kind of set.

--- analysis/timings/run_all.zsh
+++ analysis/timings/run_all.zsh
@@ -7,7 +7,7 @@ set -e
 echo "set,swaps,log2_capacity,init,param,psynth,pcrypto,ver"
 
 c=20
-for t in 1 1000 2000; do
+for t in 1 500 1000 1500; do
     for set in merkle rsa; do
         # Filter out parameter generation log message with "points" in it
         ${0:a:h}/../../target/release/examples/set_proof $set $t $c | rg -v points;
