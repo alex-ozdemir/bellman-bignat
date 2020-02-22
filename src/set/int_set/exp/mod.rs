dbce97ddc9e9aa1b6842242bce0009b1c79f0db9
Alex Ozdemir <aozdemir@hmc.edu>
2020-02-22 15:41:57 -0800

    Document Exponentiator

--- src/set/int_set/exp/mod.rs
+++ src/set/int_set/exp/mod.rs
@@ -8,6 +8,8 @@ pub mod parallel;
 pub mod serial;
 
 pub trait Exponentiator<G: SemiGroup>: Sized + Clone + Eq + Debug {
+    /// Construct an exponentiator, given the group (which includes a generator)
     fn from_group(g: G) -> Self;
+    /// Compute (generator)^(product(powers))
     fn exponentiate(&mut self, powers: Vec<Integer>) -> G::Elem;
 }
