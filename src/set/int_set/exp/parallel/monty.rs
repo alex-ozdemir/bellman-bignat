3c7c047dc339b928da05b0b0ac656811379dcc0f
Alex Ozdemir <aozdemir@hmc.edu>
2020-02-22 15:38:26 -0800

    Implement mp monty REDC, use it.
    Benchmarks show that we're loosing to GMP, still.

--- src/set/int_set/exp/parallel/monty.rs
+++ src/set/int_set/exp/parallel/monty.rs
@@ -1,10 +1,12 @@
 #![allow(non_snake_case)]
 
+use bitintr::Mulx;
 use gmp_mpfr_sys::gmp::limb_t;
-use serde::{Deserialize, Serialize};
 use rug::Integer;
+use serde::{Deserialize, Serialize};
 
 use std::mem::size_of;
+use std::ptr::{copy, write_bytes};
 
 use super::parallel_product::borrow_digits;
 
@@ -39,7 +41,7 @@ impl MontyConstants {
         let RN = R * &N;
         let B = Integer::from(1) << b;
         let N_inv_neg_B = (-N.clone()).invert(&B).unwrap();
-        assert!(N_inv_neg_B.significant_bits() < b);
+        assert!(N_inv_neg_B.significant_bits() <= b);
         Self {
             N,
             RmodN,
@@ -54,7 +56,7 @@ impl MontyConstants {
 
     /// This implementation of Montgomery reduction uses GMP instead of implementing the
     /// multiprecision algorithm. The thought is that GMP will do a better job. We'll see.
-    fn redc(&self, mut T: Integer) -> Integer {
+    pub fn redc(&self, mut T: Integer) -> Integer {
         debug_assert!(&T < &self.RN);
         debug_assert!(&0 <= &T);
         let mut m = T.clone();
@@ -72,9 +74,49 @@ impl MontyConstants {
     }
 
     /// This is an implementation of multiprecision Montgomery reduction.
-    #[allow(dead_code)]
-    fn redc_mp(&self, _T: Integer) -> Integer {
-        unimplemented!()
+    pub fn redc_mp(&self, mut Ti: Integer) -> Integer {
+        let Np = self.N_inv_neg_B;
+        let N = borrow_digits(&self.N);
+        let r = self.r as usize;
+        let p = N.len();
+
+        // We need to grow the integer to be at least as big as N, and then an extra space for any
+        // carry.
+        let min_T_size = r + p + 1;
+        Ti.reserve(size_of::<limb_t>() * 8 * min_T_size - Ti.significant_bits() as usize);
+        debug_assert!(Ti.capacity() >= size_of::<limb_t>() * 8 * min_T_size);
+
+        unsafe {
+            let T: *mut limb_t = (*Ti.as_raw_mut()).d;
+            write_bytes(T.add(Ti.significant_digits::<limb_t>()), 0, min_T_size - Ti.significant_digits::<limb_t>());
+            for i in 0..r {
+                let mut c = 0;
+                let m = limb_t::mulx(*T.add(i), Np).0;
+                for j in 0..p {
+                    let x =
+                        *T.add(i + j) as u128 + m as u128 * *N.get_unchecked(j) as u128 + c as u128;
+                    *T.add(i + j) = x as limb_t;
+                    c = (x >> (8 * size_of::<limb_t>())) as limb_t;
+                }
+                for j in p..(min_T_size - i) {
+                    let x = *T.add(i + j) as u128 + c as u128;
+                    *T.add(i + j) = x as limb_t;
+                    c = (x >> (8 * size_of::<limb_t>())) as limb_t;
+                }
+                debug_assert_eq!(*T.add(i), 0);
+            }
+            let size = (1..=p)
+                .rev()
+                .find(|s| *T.add(r + s - 1) != 0)
+                .unwrap_or(0);
+            copy(T.add(r), T, size);
+            (*Ti.as_raw_mut()).size = size as i32;
+        }
+        if Ti > self.N {
+            Ti - &self.N
+        } else {
+            Ti
+        }
     }
 
     pub fn in_to(&self, i: Integer) -> MontyNum {
@@ -95,14 +137,113 @@ impl MontyConstants {
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize, PartialEq, Eq)]
-pub struct MontyNum(Integer);
+pub struct MontyNum(pub Integer);
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use test::*;
     use rug::rand::RandState;
     use std::str::FromStr;
     const RSA_2048: &str = "25195908475657893494027183240048398571429282126204032027777137836043662020707595556264018525880784406918290641249515082189298559149176184502808489120072844992687392807287776735971418347270261896375014971824691165077613379859095700097330459748808428401797429100642458691817195118746121515172654632282216869987549182422433637259085141865462043576798423387184774447920739934236584823824281198163815010674810451660377306056201619676256133844143603833904414952634432190114657544454178424020924616515723350778707749817125772467962926386356373289912154831438167899885040445364023527381951378636564391212010397122822120720357";
+
+    /// Random operations on an integer, more or less.
+    fn operate(i: &mut Integer) {
+        let n = Integer::from_str(
+            "45032408257293263260773906994099777552173939956523527117391050121724017348167",
+        )
+        .unwrap();
+        *i += &n;
+        *i *= &n;
+        *i %= &n;
+        *i <<= 16;
+        *i /= &n;
+    }
+
+    #[test]
+    fn redc_equivalence_tiny() {
+        let n = Integer::from_str("14003342538859904917").unwrap();
+        let monty = MontyConstants::new(n.clone());
+        let input = Integer::from(1);
+
+        // Use both versions of REDC, assert result equivalence
+        let mut expect = monty.redc(input.clone());
+        let mut result = monty.redc_mp(input);
+        assert_eq!(expect, result);
+
+        // Do subsequent operations, assert result equivalence
+        // Important because redc_mp fiddles with internals.
+        // A bug could want to mess them up.
+        operate(&mut expect);
+        operate(&mut result);
+        assert_eq!(expect, result);
+    }
+
+    #[test]
+    fn redc_equivalence_small() {
+        let n = Integer::from_str("318475643993954790644818855069112088563").unwrap();
+        let monty = MontyConstants::new(n.clone());
+        let input = Integer::from(1);
+
+        // Use both versions of REDC, assert result equivalence
+        let mut expect = monty.redc(input.clone());
+        let mut result = monty.redc_mp(input);
+        assert_eq!(expect, result);
+
+        // Do subsequent operations, assert result equivalence
+        // Important because redc_mp fiddles with internals.
+        // A bug could want to mess them up.
+        operate(&mut expect);
+        operate(&mut result);
+        assert_eq!(expect, result);
+    }
+
+    #[test]
+    fn redc_equivalence_medium() {
+        let n = Integer::from_str(
+            "109910236399443861266704537898162531048702512880173728452112705364178785050797",
+        )
+        .unwrap();
+        let monty = MontyConstants::new(n.clone());
+        let input = Integer::from_str(
+            "45032408257293263260773906994099777552173939956523527117391050121724017348167",
+        )
+        .unwrap();
+
+        // Use both versions of REDC, assert result equivalence
+        let mut expect = monty.redc(input.clone());
+        let mut result = monty.redc_mp(input);
+        assert_eq!(expect, result);
+
+        // Do subsequent operations, assert result equivalence
+        // Important because redc_mp fiddles with internals.
+        // A bug could want to mess them up.
+        operate(&mut expect);
+        operate(&mut result);
+        assert_eq!(expect, result);
+    }
+
+    #[test]
+    fn redc_equivalence_large() {
+        let mut rnd = RandState::new();
+        const BITS: u32 = 2047;
+        _seed_rng(&mut rnd);
+        let monty = MontyConstants::new(Integer::from_str(RSA_2048).unwrap());
+
+        let input = Integer::from(Integer::random_bits(BITS, &mut rnd));
+
+        // Use both versions of REDC, assert result equivalence
+        let mut expect = monty.redc(input.clone());
+        let mut result = monty.redc_mp(input);
+        assert_eq!(expect, result);
+
+        // Do subsequent operations, assert result equivalence
+        // Important because redc_mp fiddles with internals.
+        // A bug could want to mess them up.
+        operate(&mut expect);
+        operate(&mut result);
+        assert_eq!(expect, result);
+    }
 
     #[test]
     fn in_out() {
@@ -155,4 +296,66 @@ mod tests {
         ));
     }
 
+    #[bench]
+    fn bench_redc(b: &mut Bencher) {
+        let mut rnd = RandState::new();
+        const BITS: u32 = 2048;
+        _seed_rng(&mut rnd);
+        let n = Integer::from_str(RSA_2048).unwrap();
+        let monty = MontyConstants::new(n.clone());
+        let mut input = Integer::from(Integer::random_bits(BITS, &mut rnd));
+        input.reserve(10000);
+        let expected = monty.redc_mp(input.clone());
+        b.iter(|| assert_eq!(expected, monty.redc(input.clone())));
+    }
+
+    #[bench]
+    fn bench_redc_mp(b: &mut Bencher) {
+        let mut rnd = RandState::new();
+        const BITS: u32 = 2048;
+        _seed_rng(&mut rnd);
+        let n = Integer::from_str(RSA_2048).unwrap();
+        let monty = MontyConstants::new(n.clone());
+        let mut input = Integer::from(Integer::random_bits(BITS, &mut rnd));
+        input.reserve(10000);
+        let expected = monty.redc(input.clone());
+        b.iter(|| assert_eq!(expected, monty.redc_mp(input.clone())));
+    }
+
+    #[bench]
+    fn bench_gmp_mult(b: &mut Bencher) {
+        let mut rnd = RandState::new();
+        const BITS: u32 = 2048;
+        _seed_rng(&mut rnd);
+        let n = Integer::from_str(RSA_2048).unwrap();
+        let input1 = Integer::from(Integer::random_bits(BITS, &mut rnd));
+        let input2 = Integer::from(Integer::random_bits(BITS, &mut rnd));
+        let expected = input1.clone() * &input2 % &n;
+        b.iter(|| assert_eq!(expected, input1.clone() * &input2 % &n));
+    }
+
+    #[bench]
+    fn bench_monty_redc_mult(b: &mut Bencher) {
+        let mut rnd = RandState::new();
+        const BITS: u32 = 2048;
+        _seed_rng(&mut rnd);
+        let n = Integer::from_str(RSA_2048).unwrap();
+        let monty = MontyConstants::new(n.clone());
+        let input1 = monty.in_to(Integer::from(Integer::random_bits(BITS, &mut rnd)));
+        let input2 = monty.in_to(Integer::from(Integer::random_bits(BITS, &mut rnd)));
+        let expected = monty.mult(input1.clone(), &input2);
+        b.iter(|| assert_eq!(expected, monty.mult(input1.clone(), &input2)));
+    }
+
+    #[bench]
+    fn bench_monty_redc_mp_mult(b: &mut Bencher) {
+        let mut rnd = RandState::new();
+        const BITS: u32 = 2048;
+        _seed_rng(&mut rnd);
+        let n = Integer::from_str(RSA_2048).unwrap();
+        let monty = MontyConstants::new(n.clone());
+        let input1 = monty.in_to(Integer::from(Integer::random_bits(BITS, &mut rnd)));
+        let input2 = monty.in_to(Integer::from(Integer::random_bits(BITS, &mut rnd)));
+        b.iter(|| monty.redc(input1.0.clone() * &input2.0));
+    }
 }
